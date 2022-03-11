98e972849c2be33c20d5fce2ae32a4b1217098fe
Prakash Dubey <prakashdubey1999@gmail.com>
2022-03-11 20:28:27 +0530

    Time: 11 ms (52.08%), Space: 11.8 MB (6.63%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+int getNumberOfNodes(ListNode* head) {
+    int cnt = 0;
+    ListNode *q = head;
+    while (q != nullptr) {
+        cnt++;
+        q = q->next;
+    }
+    return cnt;
+}
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head) return head;
+        int n = getNumberOfNodes(head);
+        // cout << "numberOfNodes: " << n << "\n";
+        k = k % n;
+        if (!k) return head;
+        ListNode *q = head, *r = head;
+        int skip = k-1;
+        while(skip>0) {
+            r = r->next;
+            skip--;
+        }
+        while (r != nullptr && r->next != nullptr) {
+            q = q->next;
+            r = r->next;
+        }
+        // cout << "r->val: " << r->val << "\n";
+        r->next = head;
+        // cout << "r->next->val: " << r->next->val << "\n";
+        ListNode* it = q;
+        while (n-- > 1) {
+            it = it->next;
+        }
+        // cout << "it->val: " << it->val << "\n";
+        it->next = nullptr;
+        return q;
+    }
+};
