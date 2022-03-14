36602cb621d5635b2ed6d0a456d28bf98d9787c3
Prakash Dubey <prakashdubey1999@gmail.com>
2022-03-14 19:38:09 +0530

    Time: 27 ms (6.05%), Space: 9.6 MB (39.77%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -1,31 +1,4 @@
 class Solution {
-    void populateStackFromVector(vector<string>& v, stack<string>& st) {
-        for (auto x: v) {
-                if (x == "." || (x == ".." && st.empty())) {
-                    continue;
-                } else if (x == "..") {
-                    st.pop();
-                } else {
-                    st.push(x);
-                }
-            }
-    }
-    void parsePathToDirectories(string& path, vector<string>& v) {
-        string tmp;
-        int n = path.size();
-        for (int i = 0; i < n; ++i) {
-            if (path[i] != '/') {
-                tmp += path[i];
-            } else {
-                if (!tmp.empty())
-                    v.push_back(tmp);
-                tmp = "";
-            }
-        }
-        if (!tmp.empty()) {
-            v.push_back(tmp);
-        }
-    }
 public:
     string simplifyPath(string path) {
         path += "/";
