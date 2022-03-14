002e5b09f5c932eca314f877f67a648f79dd33cb
Prakash Dubey <prakashdubey1999@gmail.com>
2022-03-14 18:58:03 +0530

    Time: 8 ms (62.74%), Space: 10.4 MB (36.52%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -17,10 +17,8 @@ public:
         if (!tmp.empty()) {
             v.push_back(tmp);
         }
-            tmp="";
-        
+        tmp="";
         for (auto x: v) {
-            cout << "x = " << x << "\n";
             if (x == "." || (x == ".." && st.empty())) {
                 continue;
             } else if (x == "..") {
@@ -29,16 +27,11 @@ public:
                 st.push(x);
             }
         }
-                cout << "stack size = " << st.size() << "\n";
-
         while (!st.empty()) {
             tmp = st.top() + "/" + tmp;
-                    cout << "tmp = " << tmp << "\n";
-
             st.pop();
         }
         tmp = "/" + tmp;
-        cout << "tmp = " << tmp << "\n";
         return (tmp=="/") ? "/" : tmp.substr(0,tmp.size()-1);
     }
 };
