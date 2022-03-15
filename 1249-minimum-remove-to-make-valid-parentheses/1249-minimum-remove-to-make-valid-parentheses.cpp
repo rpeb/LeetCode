a740f6fb865ff70c1b1dcdcb0d86eafa261773a3
Prakash Dubey <prakashdubey1999@gmail.com>
2022-03-15 20:18:19 +0530

    Time: 47 ms (22.63%), Space: 10.9 MB (77.91%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         int n = s.size();
+        // keep the indices to be removed from s
         stack<int> st;
         char c = 'a';
-        string res;
         for (int i = 0; i < n; ++i) {
             c = s[i];
+            // index of open paren is stored in stack
             if (c == '(') {
                 st.push(i);
             } else if (c == ')') {
+                // to handle cases like "))(("
+                // where closing paren should only pop opening paren
                 if (!st.empty() && s[st.top()] == '(') {
                     st.pop();
-                    // res.push_back(c);
                 } else {
                     st.push(i);
                 }
             }
         }
         while (!st.empty()) {
+            // remove redundant parens from original string
             s.erase(st.top(),1);
             st.pop();
         }
