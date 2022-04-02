ed911258eb161376b58ff4fd022c6b74c74f27b9
Prakash Dubey <prakashdubey1999@gmail.com>
2022-04-02 09:53:43 +0530

    Time: 10 ms (53.00%), Space: 7.6 MB (86.88%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
@@ -9,27 +9,35 @@ class Solution {
     }
 public:
     string reverseVowels(string s) {
-        // cout << "isVowel('a') = " << isVowel('a') << "\nisVowel('b') = " << isVowel('b') << "\nisVowel('u') = " << isVowel('u') << "\n";
+        
+        // pointers
         int l = 0, r = s.size() - 1;
+        
+        // storage vars
         char t;
         bool isVowelLeft = false, isVowelRight = false;
+        
         while (l < r) {
-                // cout << "l = " << l << ", r = " << r << "\n";
-            // cout << "s = " << s << "\n";
+                
             isVowelLeft = isVowel(s[l]);
             isVowelRight = isVowel(s[r]);
             
             if (isVowelLeft && isVowelRight) {
+                // swap s[l] and s[r]
                 t = s[l];
                 s[l] = s[r];
                 s[r] = t;
+                // move the pointers
                 l++;
                 r--;
             } else if (isVowelLeft) {
+                // find a vowel on the right, staying at vowel on left
                 r--;
             } else if (isVowelRight) {
+                // find a vowel on the left, staying at vowel on right
                 l++;
             } else {
+                // no vowels on l or r, so move the pointers
                 l++;
                 r--;
             }
