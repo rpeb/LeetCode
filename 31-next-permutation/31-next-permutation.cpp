2897c0747b8e530ae3a987abccbb25595c6fb34c
Prakash Dubey <prakashdubey1999@gmail.com>
2022-04-03 23:22:54 +0530

    Time: 4 ms (73.50%), Space: 12 MB (93.15%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return;
+        int i = n-1;
+        while (i > 0 && nums[i-1] >= nums[i]) {
+            i--;
+        }
+        if (i == 0) {
+            reverse(nums.begin(), nums.end());
+        } else {
+            // cout << "i = " << i << "\n";
+            int j = i;
+            i--;
+            int mx = INT_MAX, g = i;
+            while (j < n) {
+                if (nums[j] > nums[i] && nums[j] <= mx) {
+                    mx = nums[j];
+                    g = j;
+                }
+                j++;
+            }
+            // cout << "i = " << i << ", g = " << g << "\n";
+            swap(nums[i], nums[g]);
+            reverse(nums.begin() + i + 1, nums.end());
+        }
+    }
+};
