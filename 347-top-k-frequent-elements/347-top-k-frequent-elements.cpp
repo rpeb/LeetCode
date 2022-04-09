3927b993a0019caa7c273237131d53ac3a482f9a
Prakash Dubey <prakashdubey1999@gmail.com>
2022-04-09 09:57:34 +0530

    Time: 21 ms (54.49%), Space: 13.7 MB (71.77%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -6,19 +6,27 @@ public:
             nmap[num]++;
         }
         
+        // to store pair of frequency and num,
+        // and find k-largest frequencies
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minq;
         
         for (auto it = nmap.begin(); it != nmap.end(); ++it) {
+            // forming pair from values in map
             int num = it->first;
             int frequency = it->second;
             pair<int,int> p = {frequency, num};
+            
+            // push pair in min_queue
             minq.push(p);
+            
+            // only store k (max or min guaranteed by heap)
             if (minq.size() > k) {
                 minq.pop();
             }
         }
         
         vector<int> res;
+        
         while (!minq.empty()) {
             res.push_back(minq.top().second);
             minq.pop();
