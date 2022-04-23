3ccc71c851b6cb7d023a161d324ecfdb4b13cc9d
Prakash Dubey <prakashdubey1999@gmail.com>
2022-04-23 11:30:17 +0530

    Time: 15 ms (5.27%), Space: 8.9 MB (5.80%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    vector<list<pair<int,int>>> map;
+    
+public:
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        map.resize(10);
+        string encoded;
+        int n = longUrl.size();
+        for (int i = 0; i < n; ++i) {
+            char x = longUrl[i];
+            // cout << "x = " << x << "\n";
+            int hash = ((x - 'a') + 97) % 10;
+            encoded += to_string(hash);
+            map[hash].push_back({i,x});
+        }
+        cout << encoded << "\n";
+        return encoded;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        int n = shortUrl.size();
+        string decoded;
+        for (int i = 0; i < n; ++i) {
+            int idx = shortUrl[i] - '0';
+            decoded += map[idx].front().second;
+            map[idx].pop_front();
+        }
+        return decoded;
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
