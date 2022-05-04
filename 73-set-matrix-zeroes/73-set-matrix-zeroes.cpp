02a6f397224ddacef6da3b0d6d27341b31baa814
Prakash Dubey <prakashdubey1999@gmail.com>
2022-05-04 08:40:02 +0530

    Time: 17 ms (60.23%), Space: 13.2 MB (54.80%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,28 +1,54 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        queue<pair<int,int>> q;
         int m = matrix.size();
         int n = matrix[0].size();
-        // find all the zero positions and insert the indices in a queue
+        
+        // first row and column -> to be handled in end
+        bool topRowZero = false;
+        bool leftColumnZero = false;
+        for (int i = 0; i < n; ++i) {
+            if (matrix[0][i] == 0) {
+                topRowZero = true;
+                break;
+            }
+        }
         for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+            if (matrix[i][0] == 0) {
+                leftColumnZero = true;
+                break;
+            }
+        }
+        
+        // start from 2nd row and column and create zero markers
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
                 if (matrix[i][j] == 0) {
-                    q.push({i,j});
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-        while (!q.empty()) {
-            pair<int,int> p = q.front();
-            int x = p.first;
-            int y = p.second;
-            for (int i = 0; i < m; ++i) {
-                matrix[i][y] = 0;
+        
+        // again traverse matrix and set zeros using markers
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
             }
-            for (int j = 0; j < n; ++j) {
-                matrix[x][j] = 0;
-            }
-            q.pop();
         }
-     }
+        
+        // handle top row and left column now
+        if (topRowZero) {
+            for (int i = 0; i < n; ++i) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (leftColumnZero) {
+            for (int i = 0; i < m; ++i) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
 };
