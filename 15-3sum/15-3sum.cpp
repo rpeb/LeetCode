3d38b30167d51a9eea06b08aa9cccd91b02d65f8
Prakash Dubey <prakashdubey1999@gmail.com>
2022-05-11 21:18:34 +0530

    Time: 90 ms (78.93%), Space: 20 MB (87.28%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -9,12 +9,10 @@ public:
             if (i > 0 && nums[i] == nums[i-1]) continue;
             int j = i + 1;
             int k = n - 1;
-            vector<int> temp;
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0) {
-                    temp = {nums[i],nums[j],nums[k]};
-                    res.push_back(temp);
+                    res.push_back(vector<int> {nums[i],nums[j],nums[k]});
                     while (j < k && nums[j] == nums[j+1]) j++;
                     while (k > j && nums[k-1] == nums[k]) k--;
                     if (j < n-1) j++;
