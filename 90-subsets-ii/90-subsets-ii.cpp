042c2c173a8633a23a395dd61fb5b6f151e68969
Prakash Dubey <prakashdubey1999@gmail.com>
2022-05-27 22:26:37 +0530

    Time: 4 ms (66.71%), Space: 7.8 MB (49.70%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> subsets;
-        subsets.push_back({});
-        vector<int> tmp;
-        for (int i = 0; i < n; ++i) {
-            int subsetSize = subsets.size();
-            for (int j = 0; j < subsetSize; ++j) {
-                tmp = subsets[j];
-                tmp.push_back(nums[i]);
-                if (count(subsets.begin(),subsets.end(),tmp) == 0)
-                    subsets.push_back(tmp);
-            }
+    void solve(vector<int>& nums, vector<int> temp, int idx, vector<vector<int>>& res) {
+        // base case
+        res.push_back(temp);
+        
+        for (int i = idx; i < nums.size(); ++i) {
+            if (idx != i && nums[i] == nums[i-1]) continue;
+            // choose
+            temp.push_back(nums[i]);
+            solve(nums,temp,i+1,res);
+            // unpick
+            temp.pop_back();
         }
-        return subsets;
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int idx = 0;
+        vector<vector<int>> res;
+        vector<int> temp;
+        solve(nums, temp, idx, res);
+        return res;
     }
 };
