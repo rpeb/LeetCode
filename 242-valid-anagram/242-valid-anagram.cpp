744b937d360d4a667cbed86d1ee8fb426ea2c196
Prakash Dubey <prakashdubey1999@gmail.com>
2022-07-28 12:43:45 +0530

    Time: 14 ms (57.46%), Space: 7.3 MB (45.59%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    void printVec(vector<int>& freq) {
+        for (int i = 0; i < 26; ++i) {
+            cout << "i = " << (char)(i+97) << ", freq[i] = " << freq[i] << "\n";
+        }
+    }
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        int n1 = s.size(), n2 = t.size();
+        vector<int> m(26, 0);
+        for (int i = 0; i < n1; ++i) {
+            m[s[i]-'a']++;
+        }
+        printVec(m);
+        for (int i = 0; i < n2; ++i) {
+            if (m[t[i]-'a'] > 0) {
+                m[t[i]-'a']--;
+            }
+            else
+                return false;
+        }
+        for (int i = 0; i < 26; ++i) {
+            if (m[i] != 0)
+                return false;
+        }
+        cout << "========================\n";
+        printVec(m);
+        return true;
+    }
+};
