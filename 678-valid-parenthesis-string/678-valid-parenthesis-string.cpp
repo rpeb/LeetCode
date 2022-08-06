ed991bbbe715dc1e538a886f83bacde51fc42b6d
Prakash Dubey <prakashdubey1999@gmail.com>
2022-08-06 10:51:31 +0530

    Time: 2 ms (52.76%), Space: 6.4 MB (19.92%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int lo = 0, hi = 0;
-        for (int i = 0; i < s.size(); ++i) {
+        stack<int> openParen;
+        stack<int> star;
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
             char c = s[i];
-            
             if (c == '(') {
-                lo++;
-                hi++;
-            } else if (c == ')') {
-                if (lo > 0) lo--;
-                hi--;
+                openParen.push(i);
+            } else if (c == '*') {
+                star.push(i);
             } else {
-                if (lo > 0) lo--;
-                hi++;
-            }
-            
-            // 1. if number of ) is more than number of * and (, hi becomes negative
-            // 2. ) can only be balanced by * or ( in front of it
-            //    so even if we have more *, ( later on and hi becomes positive,
-            //    we already have an invalid substring that cannot be made valid
-            //
-            // 3. *(*))),(*)) : the substring before comma is already invalid and hi < 0                                     there, since it makes whole string invalid we return false
-            
-            
-            if (hi < 0) {
-                return false;
+                if (openParen.empty() && star.empty()) return false;
+                if (!openParen.empty()) {
+                    openParen.pop();
+                } else {
+                    star.pop();
+                }
             }
         }
-        
-        
-        return lo == 0;
+        while (!openParen.empty() && !star.empty()) {
+            if (openParen.top() > star.top()) {
+                return false;
+            }
+            openParen.pop();
+            star.pop();
+        }
+        return openParen.empty();
     }
 };
