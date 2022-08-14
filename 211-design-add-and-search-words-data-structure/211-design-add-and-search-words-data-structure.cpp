f70c3ca8349c9378d47f52ee2b70841dcf323d39
Prakash Dubey <prakashdubey1999@gmail.com>
2022-08-14 13:40:20 +0530

    Time: 2196 ms (29.38%), Space: 634.9 MB (5.09%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -38,6 +38,34 @@ class WordDictionary {
         }
         return curr->endOfWord;
     }
+    
+    bool searchHelper1(string word, int idx, TrieNode* root) {
+        if (root and idx == word.size())
+            return root->endOfWord;
+        
+        if (!root)
+            return false;
+        int n = word.size();
+        TrieNode* curr = root;
+        bool temp = false;
+        for (int i = idx; i < n; ++i) {
+            if (word[i] != '.') {
+                if (!curr->next[word[i] - 'a'])
+                    return false;
+                curr = curr->next[word[i]-'a'];
+            } else {
+                for (int j = 0; j < 26; ++j) {
+                    if (curr->next[j]) {
+                        temp = searchHelper1(word, i+1, curr->next[j]);
+                    }
+                    if (temp)
+                        return true;
+                }
+                return false;
+            }
+        }
+        return curr->endOfWord;
+    }
 public:
     WordDictionary() {
         root = new TrieNode();
@@ -57,7 +85,8 @@ public:
     }
     
     bool search(string word) {
-        return searchHelper(root, 0, word);
+        // return searchHelper(root, 0, word);
+        return searchHelper1(word, 0, root);
     }
 };
 
