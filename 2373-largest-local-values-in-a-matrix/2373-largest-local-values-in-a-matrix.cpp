58ad583dfc7c3bd3bb429d0c7dd209f42db7cf07
Prakash Dubey <prakashdubey1999@gmail.com>
2022-08-16 10:33:25 +0530

    Time: 23 ms (71.43%), Space: 11.1 MB (85.71%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cpp
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cpp
@@ -1,24 +1,19 @@
 class Solution {
-    int findMaxSurround(vector<vector<int>>& grid,int x,int y) {
-        int row[] = {-1,-1,-1,0,0,0,1,1,1};
-        int col[] = {-1,0,1,-1,0,1,-1,0,1};
-        int mx = grid[x][y];
-        for (int i = 0; i < 9; ++i) {
-            mx = max(mx,grid[x + row[i]][y + col[i]]);
-        }
-        return mx;
-    }
 public:
     vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = m;
-        vector<vector<int>> res;
+        vector<vector<int>> res(m-2, vector<int>(n-2));
         for (int i = 1; i < m - 1; ++i) {
-            vector<int> row(m-2);
             for (int j = 1; j < n - 1; ++j) {
-                row[j-1] = findMaxSurround(grid,i,j);
+                int mx = 0;
+                for (int r = i-1; r <= i + 1; ++r) {
+                    for (int c = j-1; c <= j + 1; ++c) {
+                        mx = max(mx, grid[r][c]);
+                    }
+                }
+                res[i-1][j-1] = mx;
             }
-            res.push_back(row);
         }
         return res;
     }
