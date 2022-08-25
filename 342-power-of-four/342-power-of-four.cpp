0b93039271df010ebdec42594381df06fa9a8bd8
Prakash Dubey <prakashdubey1999@gmail.com>
2022-08-25 11:06:13 +0530

    Time: 4 ms (40.16%), Space: 6.1 MB (6.02%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,11 +1,15 @@
 class Solution {
-    // int base;
+    
 public:
     bool isPowerOfFour(int n) {
-        while (n > 1) {
-            if (n % 4) return false;
-            n /= 4;
+        if (n == 0) return false;
+        double lognBase2 = log2(n);
+        cout << "lognBase2 = " << ceil(lognBase2) << "\n";
+        int log4Base2 = 2;
+        if (ceil(lognBase2) == floor(lognBase2)) {
+            return (int)lognBase2 % log4Base2 == 0;
+        } else {
+            return false;
         }
-        return n == 1;
     }
 };
