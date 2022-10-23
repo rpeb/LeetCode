f5de7bd8cd4c55086f9792946002c0b7de8d9c22
Prakash Dubey <prakashdubey1999@gmail.com>
2022-10-23 22:11:32 +0530

    Time: 185 ms (9.97%), Space: 33 MB (13.56%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -7,14 +7,11 @@ public:
         for (int &num : nums) {
             sum += num;
         }
-        // sum = 9
         set<int> numsSet(nums.begin(), nums.end());
         int sumOfDistinct = 0;
         for (int num: numsSet) {
             sumOfDistinct += num;
         }
-        // sumOfDistinct = 7
-        cout << "sum = " << sum << "\n" << "sum1toN = " << sum1toN << "\n";
         int repeated = sum - sumOfDistinct;
         int missing = sum1toN - sumOfDistinct;
         return {repeated, missing};
